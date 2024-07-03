23c3f1a8b064cdbafbf6f39565d4d0944f8f0291
pruthvi-dot <pruthvipediredlainit@outlook.com>
2024-07-03 16:53:49 -0400

    new terraform code

--- module/app1/main.tf
+++ module/app1/main.tf
@@ -4,6 +4,22 @@ module "key" {
   key_name = "serge-key"
 }
 
-output "key-name" {
-  value = aws_key_pair.ec2_key.key_name
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "terraform module"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+    Team = Devops 
+  }
 }
